bc69bfbce06b514d57210782d152dc05caf7a593
Abduraimov Mo'minjon <DelphiUz@users.noreply.github.com>
2018-01-09 20:38:03 +0500

    Update Profile Image Checker.pas

--- Profile Image Checker.pas
+++ Profile Image Checker.pas
@@ -1,38 +1,34 @@
-﻿program tmp;
-
-
+﻿program profileImageChecker;
 
 function main:integer;
 var
   i, i : integer;
 begin
-int l,w[1000],h[1000],n;
-Readln(l);
-Readln(n);
-for i := 0 to n-1 do 
-begin 
-Readln(&w[i],&h[i]);
-end; 
-for i := 0 to n-1 do 
-begin 
-if w[i]>l  and  h[i]>l  and  w[i]<>h[i] then  or  (w[i]>l  and  h[i]=l)  or  (w[i]=l  and  h[i]>l) then 
-Writeln('CROP IT');
-else if(w[i]<l  or  h[i]<l  or  (w[i]>l  and  h[i]<l)  or  (w[i]<l  and  h[i]>l))
-Writeln('UPLOAD ANOTHER');
-else if((w[i]=l  and  h[i]=l)  or  w[i]=h[i]  or  (w[i]>l  and  h[i] >l  and  w[i]=h[i]))
-Writeln('ACCEPTED');
-end; 
-Result := 0;
+  int l,w[1000],h[1000],n;
+  Readln(l);
+  Readln(n);
+  for i := 0 to n-1 do 
+  begin 
+    Readln(&w[i],&h[i]);
+  end; 
+  for i := 0 to n-1 do 
+  begin 
+    if w[i]>l  and  h[i]>l  and  w[i]<>h[i] then  or  (w[i]>l  and  h[i]=l)  or  (w[i]=l  and  h[i]>l) then 
+      Writeln('CROP IT');
+    else if(w[i]<l  or  h[i]<l  or  (w[i]>l  and  h[i]<l)  or  (w[i]<l  and  h[i]>l))
+      Writeln('UPLOAD ANOTHER');
+    else if((w[i]=l  and  h[i]=l)  or  w[i]=h[i]  or  (w[i]>l  and  h[i] >l  and  w[i]=h[i]))
+      Writeln('ACCEPTED');
+  end; 
+  
+  Result := 0;
 end;
 
-
-
-
-begin
+Begin
   try
     Main;
   except
     on e:Exception do
       WriteLn(e.Message);
   end;
-end.
+End.
