631e64fdb2639387b3530f4e42e8528ffad41bf7
Abduraimov Mo'minjon <DelphiUz@users.noreply.github.com>
2018-05-05 15:18:03 +0500

    Add files via upload

---
+++ BeginnerTasksAnswers/Begin_37.pas
@@ -0,0 +1,16 @@
+Program Begin_37;
+
+Var
+  V1, V2, T, s: Real;
+  
+Begin
+  Write('V1 = '); ReadLn(V1);
+  Write('V2 = '); ReadLn(V2);
+  Write('s = '); ReadLn(s);
+  Write('T = '); ReadLn(T);
+
+  WriteLn;
+  
+  Write('S = ', (V1*T + V2*T) - S , 'km ');
+  ReadLn;
+End.  
