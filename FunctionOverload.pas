eb71c81fb278f3facaede52cd4b46c781958c24e
Abduraimov Mo'minjon <DelphiUz@users.noreply.github.com>
2018-06-04 15:25:59 +0500

    Add files via upload

---
+++ FunctionOverload.pas
@@ -0,0 +1,31 @@
+Do you have what it takes to freelance as a software developer?
+
+Subscriber Anu isn't sure he's ready:
+
+I have been working in the field of programming for 2 years. I would like to work as a freelancer, but I don't have the confidence to work on projects independently. As a programmer how can I improve?
+
+Anu, you're falling into a trap here.
+
+There's a little voice in your head that says, "You? Freelancing? That's crazy. You don't have the skills and experience. Maybe someday you will, but not yet."
+
+If you're holding off for that voice to tell you, "OK, now you're ready!" then you're in for a long wait.
+
+Fact:
+
+You will NEVER know everything you "need" to know.
+
+You will NEVER have all the skills you "need" to have.
+
+You will NEVER just wake up one day and have the confidence you "need" to have in order to make your goal a reality.
+
+In my experience I've found that I could spend 5 years honing my programming skills and in the end I'm no more confident than I was at the beginning. 
+
+What *will* increase your confidence?
+
+For me it was improving my ability to acquire new skills and knowledge quickly and efficiently to tackle whatever challenge I happened to be facing...
+
+To jump off the cliff and stitch together my parachute on the way down.
+
+Even though I may not have had complete confidence that I knew everything I'd need to know for a given project, I had total confidence in my ability to GET that knowledge.
+
+That's a game changer.
