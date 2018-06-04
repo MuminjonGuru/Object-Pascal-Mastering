a56c71195fc39bc7b977e6fc6f78df40e574630a
Abduraimov Mo'minjon <DelphiUz@users.noreply.github.com>
2018-06-04 15:26:27 +0500

    Add files via upload

---
+++ Array_with_Procedure.pas
@@ -0,0 +1,60 @@
+program Demo2_3;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils;
+
+type
+  Arr = Array [1..10, 1..10] of Integer;
+
+Var
+  I, J, N: Integer;
+  A, B, C: Arr;
+
+procedure Input(M: Integer; Var C: Arr);
+var
+  I, J: Integer;
+begin
+  for I := 1 to M do
+  for J := 1 to M do
+  ReadLn(C[I, J]);
+end;
+
+procedure Output(M: Integer; C: Arr; DefaultParam: String = ':');
+var
+  I, J: Integer;
+begin
+  DefaultParam := '--';
+  for I := 1 to M do
+    begin
+      for J := 1 to M do
+        Write(C[I, J]: 2, DefaultParam);
+        WriteLn;
+    end;
+end;
+
+begin
+  Write('N = ');  ReadLn(N);
+  WriteLn('Array A');
+  Input(N, A);
+  WriteLn('Array B');
+  Input(N, B);
+
+  for I := 1 to N do
+  for J := 1 to N do
+  C[I, J] := A[I, J] + B[I, J];
+
+  WriteLn('Array A');
+  Output(N, A);
+
+  WriteLn('Array B');
+  Output(N, B);
+
+  WriteLn('Array C');
+  Output(N, C);
+
+  ReadLn;
+end.
