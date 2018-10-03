9e683204e289b1de9503216f4f3e670e75d0f029
Abduraimov Mo'minjon <24930020+MuminjonGuru@users.noreply.github.com>
2018-10-03 14:38:45 +0500

    Add files via upload

---
+++ school economical task solved.pas
@@ -0,0 +1,17 @@
+﻿{
+  Bankka B foizli daromad olish uchun qo'yilgan A so'm pulning M yildan
+  keyingi qiymatini toping
+}
+
+program Task4;
+
+var
+  A, B, M: Real;
+  
+begin
+  A := 5000; // 5000 so'm qo'yildi
+  B := 4.5; // 4.5 foizli daromad 
+  M := A * (1 + B / 100);
+  
+  WriteLn(M:2:2);
+end.  
