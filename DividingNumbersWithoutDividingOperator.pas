00e61bc35c8963e51c94ddd338cbb15616f597e3
Abduraimov Mo'minjon <24930020+MuminjonGuru@users.noreply.github.com>
2018-11-03 23:54:50 +0500

    Add files via upload

---
+++ DividingNumbersWithoutDividingOperator.pas
@@ -0,0 +1,19 @@
+﻿program DividingNumbersWithoutDividingOperator;
+
+var
+  n, m, b: Real;
+  
+Begin
+  Write('n va m ni kiriting: ');  ReadLn(n, m);
+  
+  b := 0; // initsializatsiya
+  
+  while n >= m do
+  begin 
+    b := b + 1; 
+    n := n - m;
+  end;
+  
+  WriteLn('Butun qismi: ', b, '; Qoldiq qismi: ', n);
+
+End.  
