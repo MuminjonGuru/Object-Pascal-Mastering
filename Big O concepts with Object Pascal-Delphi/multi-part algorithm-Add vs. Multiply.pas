82d94bf7be98e2891c615f2ff547411d5e94f852
Abduraimov Mo'minjon <24930020+MuminjonGuru@users.noreply.github.com>
2018-12-15 19:10:06 +0500

    Create multi-part algorithm-Add vs. Multiply.pas

---
+++ Big O concepts with Object Pascal-Delphi/multi-part algorithm-Add vs. Multiply.pas
@@ -0,0 +1,16 @@
+// Add the Runtimes O(A + B) - Means: do this the when you're all done, do that
+for A in ArrA do
+  writeLn(A);
+  
+for B in ArrB do
+  writeLn(B);
+  
+{*******************************}  
+
+Multiply the Runtimes O(A * B) - Means: do this for each time you do that 
+for A in ArrA do
+  for B in ArrB do
+    writeLn(A ,' ', B);
+    
+/////////////////////////////////
+
