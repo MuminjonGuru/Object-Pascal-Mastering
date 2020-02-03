3959ebe1fb8c9cc58b07909477fe5ee66311c1d0
Abduraimov Muminjon <muminjonguru@outlook.com>
2020-02-03 14:49:28 +0500

    don't catch exception
    code example for an article  -  Delphi.Uz

---
+++ DontCatchException.pas
@@ -0,0 +1,25 @@
+program DontCatchException;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils;
+
+begin
+  try
+    var Num1 : Real := 2.76;
+    var Num2 : Real := -0;
+
+    WriteLn(Format('%d / %d = %d;', [Num1, Num2, Num1 / Num2]));
+
+  except
+    on E: EMathError do
+      WriteLn('EMathError');
+    on E: EZeroDivide do
+      WriteLn('EZeroDivide')
+  end;
+
+  ReadLn;
+end.
