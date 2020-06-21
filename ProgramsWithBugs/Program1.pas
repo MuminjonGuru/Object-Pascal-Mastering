d5b776e0cf9454d4bfb81fd537705f62afb02afd
Abduraimov Muminjon <muminjonguru@outlook.com>
2020-06-21 23:29:22 +0500

    BuggedProgram One

---
+++ ProgramsWithBugs/Program1.pas
@@ -0,0 +1,13 @@
+program test(input,output)
+// Three compiler errors 
+
+var
+  valuel,value2,average;
+begin
+  writeln('Enter value 1 >>');
+  readln(valuel);
+  writeln('Enter value 2 >>');
+  readln(value2);
+  average:={valuel+value2/2;
+  writeln('Average is ',average:B:2);
+end.
