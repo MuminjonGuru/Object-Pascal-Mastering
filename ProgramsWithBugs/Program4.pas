0139082669414194ca994a6c799ae1f2a87ae783
Abduraimov Muminjon <muminjonguru@outlook.com>
2020-06-21 23:34:18 +0500

    BuggedProgram Four

---
+++ ProgramsWithBugs/Program4.pas
@@ -0,0 +1,13 @@
+program test(input,output);
+// Two compiler errors 
+
+var value:real;
+
+begin
+  Writeln('Enter a Value: ');
+  Readln(Value);
+  If (Value < O)
+    Writeln('Can not calculate square root of a negative')
+  else
+    WriteLn('Square root is ', Sqrt(Val):8:2);
+end.
