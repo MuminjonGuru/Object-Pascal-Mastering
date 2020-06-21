c0e94b5093446820d067d65dd8009c15cfd26d5e
Abduraimov Muminjon <muminjonguru@outlook.com>
2020-06-21 23:36:07 +0500

    BuggedProgram Five

---
+++ ProgramsWithBugs/Program5.pas
@@ -0,0 +1,18 @@
+program test(input,output);
+// Two compiler errors
+
+var 
+  valuel,value2,largest: Real;
+
+begin
+  WriteLn('Enter two values :');
+  ReadLn(valuel,value2);
+  if (valuel>value2) then
+    Largest:=Valuel;
+  else
+    Largest:=Value2;
+    
+  WriteLn('Largest value is ', Largest:B:2);
+  
+  ReadLn; //wait
+end.
