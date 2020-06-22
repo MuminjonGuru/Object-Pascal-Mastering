e8f1fc5591d626c70a514051f88d325cddceea7a
Abduraimov Muminjon <muminjonguru@outlook.com>
2020-06-22 23:59:18 +0500

    BuggedProgram Thirteen

---
+++ ProgramsWithBugs/Program13.pas
@@ -0,0 +1,16 @@
+program test(input,output);
+(* Two compiler errors *)
+var invalue:real;
+function get real(min,max:real) :real;
+begin -
+end;
+begin
+end.
+repeat
+writeln('Enter a value>> ');
+readln(val);
+if ((val<O) or (val>max)) then
+writeln('INVALID');
+until ((val>min) and (val<max);
+invalue:=get real(O,lOO);
+writeln('Input value is ',invalue);
