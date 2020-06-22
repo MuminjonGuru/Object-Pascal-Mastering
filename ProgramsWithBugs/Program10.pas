92b98402a1756e8e032b31dc52fff427ad85e7f9
Abduraimov Muminjon <muminjonguru@outlook.com>
2020-06-23 00:03:52 +0500

    changes

--- ProgramsWithBugs/Program10.pas
+++ ProgramsWithBugs/Program10.pas
@@ -1,11 +1,16 @@
 program test(input,output);
+
 (* Two compiler errors *)
-var i,sqr_value,cube_value:real;
+var 
+  i,sqr_value,cube_value: Real;
+  
 begin
-for i:=1 to 10 do
-begin
-sqr value:=i*i;
-cude value:=i*i*i;
-writeln(i:8,sqr_value:8,cube_value:8);
-end;
+  for i:=1 to 10 do
+  begin
+    Sqr value := i*i;
+    cude value:= i*i*i;
+    writeln(i:8, sqr_value:8, cube_value:8);
+  end;
+  
+  ReadLn;
 end.
