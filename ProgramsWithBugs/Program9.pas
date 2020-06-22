c9afbe196589445af3ae8ab50a7d147df00d06c3
Abduraimov Muminjon <muminjonguru@outlook.com>
2020-06-23 00:02:34 +0500

    changes

--- ProgramsWithBugs/Program9.pas
+++ ProgramsWithBugs/Program9.pas
@@ -1,10 +1,15 @@
 program test(input,output);
 (* Two compiler errors *)
-var value:real;
+var 
+  Value: Real;
+ 
 begin
+  Repeat
+    writeln('Enter a value between 0 and 100: ');
+    readln(value);
+  until (O<value>lOO);
+  
+  WriteLn('Square root is ', Sqrt(Value));
+
+  ReadLn;
 end.
-repeat
-writeln('Enter a value between 0 and 100>>');
-readln(value);
-until (O<value>lOO);
-writeln('Square root is ',sqrt(value));
