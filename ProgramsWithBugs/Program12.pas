2b128889971772d4d19e7fbce280fc5da0d0599e
Abduraimov Muminjon <muminjonguru@outlook.com>
2020-06-22 23:58:46 +0500

    BuggedProgram Twelve

---
+++ ProgramsWithBugs/Program12.pas
@@ -0,0 +1,13 @@
+program test(input,output);
+(* One compiler error *)
+var valuel,value2,result:integer;
+function add values(a:integer) :integer;
+begin -
+add_values:=a+b;
+end;
+begin
+end.
+writeln('Enter two values>> ');
+readln(valuel,value2);
+result:=add values(valuel,value2);
+writeln('Added the result is ',result);
