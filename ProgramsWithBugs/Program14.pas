95e899b185ac92f60c003f2164d3aa5462bf6af2
Abduraimov Muminjon <muminjonguru@outlook.com>
2020-06-22 23:59:35 +0500

    Create Program14.pas

---
+++ ProgramsWithBugs/Program14.pas
@@ -0,0 +1,16 @@
+program test(input,output);
+(* Two compiler errors *)
+var invalue:real;
+procedure get real(min,max:real;var val);
+begin -
+end;
+begin
+end.
+repeat
+writeln('Enter a value>> ');
+readln(val);
+if ((val<O) or (val>max)) then
+writeln('INVALID');
+until ((val>min) and (val<max));
+get real(O,lOO,invalue);
+writeln('Input value is 'invalue);
