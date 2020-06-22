e055b32aa9d35c9a612a5d1644dfc8bea7546993
Abduraimov Muminjon <muminjonguru@outlook.com>
2020-06-23 00:10:32 +0500

    changes

--- ProgramsWithBugs/Program13.pas
+++ ProgramsWithBugs/Program13.pas
@@ -1,16 +1,23 @@
-program test(input,output);
-(* Two compiler errors *)
-var invalue:real;
-function get real(min,max:real) :real;
-begin -
-end;
+program test;
+
+// two compiler errors
+
+var
+  InValue: Real;
+  
+procedure get_real(Min, Max: Real; Var Val);
 begin
+  Repeat
+    WriteLn('Enter a value: ');
+    ReadLn(Val);
+    if((Val < 0) or (Val > Max)) then
+      WriteLn('InValid');
+  Until((Val > Min) and (Val < Max));
+end;
+
+begin
+  get_real(0, 100, InValue);
+  WriteLn(InValue);
+
+  ReadLn;
 end.
-repeat
-writeln('Enter a value>> ');
-readln(val);
-if ((val<O) or (val>max)) then
-writeln('INVALID');
-until ((val>min) and (val<max);
-invalue:=get real(O,lOO);
-writeln('Input value is ',invalue);
