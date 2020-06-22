9910c099349b9dac0269ca0d76d60d54d34d6191
Abduraimov Muminjon <muminjonguru@outlook.com>
2020-06-22 23:56:33 +0500

    BuggedProgram Nine

---
+++ ProgramsWithBugs/Program9.pas
@@ -0,0 +1,10 @@
+program test(input,output);
+(* Two compiler errors *)
+var value:real;
+begin
+end.
+repeat
+writeln('Enter a value between 0 and 100>>');
+readln(value);
+until (O<value>lOO);
+writeln('Square root is ',sqrt(value));
