53306970ddec9f9f12ce137e1d7b91e98a44e21c
Abduraimov Muminjon <muminjonguru@outlook.com>
2020-06-23 00:05:12 +0500

    changes

--- ProgramsWithBugs/Program11.pas
+++ ProgramsWithBugs/Program11.pas
@@ -1,11 +1,16 @@
 program test(input,output);
-(* Two compiler errors *)
-var i:integer;
+// Two compiler errors 
+
+var 
+  I:integer;
 begin
+ 
+  for I=1 to 10 do
+  begin
+    x:=10*I;
+    y:=4*x+1;
+    WriteLn(x:8:2, y:8:2);
+  end;
+
+  ReadLn;
 end.
-for i=1 to 10 do
-begin
-end;
-x:=10*i;
-y:=4*x+1;
-writeln(x:8:2,y:8:2);
