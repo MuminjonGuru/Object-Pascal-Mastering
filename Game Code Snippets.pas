30ed1bdc7d8b54535767af45f6430de50b20ea40
Abduraimov Muminjon <muminjonguru@outlook.com>
2020-08-13 15:06:01 +0500

    live tracker

--- Game Code Snippets.pas
+++ Game Code Snippets.pas
@@ -2,3 +2,7 @@
 const
   BoardSize: TSize = (cx: 500; cy: 500);
     
+{****************************************************}
+
+// Live updater
+LlbTrack.Caption := Format('%d, %d', [ViewOffset.X + X div 10, ViewOffset.Y + Y div 10]);
