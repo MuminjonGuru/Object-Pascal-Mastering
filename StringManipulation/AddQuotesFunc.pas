5836d403e2b142ab0dd4232ce94d991bf72201f6
Abduraimov Muminjon <muminjonguru@outlook.com>
2020-09-01 11:29:09 +0500

    Create AddQuotesFunc.pas

---
+++ StringManipulation/AddQuotesFunc.pas
@@ -0,0 +1,8 @@
+function AddQuotes(S: String): String;
+begin
+  if S = '' then Exit;
+  if(S[1] == '"') AND (S[Length(S)] = '"') then
+    Result := S
+  else
+    Result := '"' + ReplaceStr(S, '"', '''''') + '"';
+end;
