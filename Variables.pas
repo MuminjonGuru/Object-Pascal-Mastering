7e4368a9d8884d9c5e5cacf49f7f26bfbd7ebc14
Abduraimov Muminjon <muminjonguru@outlook.com>
2023-09-01 22:33:01 +0500

    Update Variables.pas

--- Variables.pas
+++ Variables.pas
@@ -1,38 +1,47 @@
 program Variables;
 
+// Declare global variables
 var
- // X, Y: Integer;
-
-  C: Char;
-
-  //X: Single;
-
-  X, Y: Integer;
-  Result: Single;
+  X, Y: Integer;   // Variables to store integer values
+  Z: Single;       // Variable to store floating point value
+  Result: Single;  // Variable to store the result of division
+  C: Char;         // Variable to store character value
 
 begin
-
+  // Demonstrate simple arithmetic
   X := 5;
-  WriteLn(X * 2);
+  WriteLn('X * 2 = ', X * 2);
 
-   X := 10;
-   Y := 5;
-   WriteLn(X + Y);
+  // Demonstrate addition with two variables
+  X := 10;
+  Y := 5;
+  WriteLn('X + Y = ', X + Y);
 
+  // Display a character
   C := '@';
-  WriteLn('Mening ismimning bosh harfi: ', C);
+  WriteLn('The first letter of my name is: ', C);
 
-  X := 1.5;
-  WriteLn(X:1:3);
+  // Assign a float value to a variable of type Single and display it
+  Z := 1.5;
+  WriteLn('Value of Z (with 3 decimal places) = ', Z:1:3);
 
+  // Read values from user and perform division
+  Write('Enter value for X: ');  
+  ReadLn(X);
+  Write('Enter value for Y: ');  
+  ReadLn(Y);
+  // Ensure Y is not zero to prevent division by zero error
+  if Y <> 0 then 
+  begin
+    Result := X / Y;
+    WriteLn(X, ' divided by ', Y, ' equals ', Result:2:2);
+  end
+  else
+  begin
+    WriteLn('Y cannot be zero.');
+  end;
 
-  Write('X = ');  ReadLn(X);
-  Write('Y = ');  ReadLn(Y);
-  Result:=X / Y;
-  WriteLn(X, ' / ', Y, ' = ', Result:2:2);
-
-
-  WriteLn('Dasturni tugatish uchun biror tugmani bosing!');
+  // Wait for user input before exiting
+  WriteLn('Press any key to exit the program...');
   ReadLn;
-
 end.
