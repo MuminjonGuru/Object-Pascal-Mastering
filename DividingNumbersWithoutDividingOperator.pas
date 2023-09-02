c284bbb571034056b37d5335f26c6cbd200f36d4
Abduraimov Muminjon <muminjonguru@outlook.com>
2023-09-02 21:23:16 +0500

    updt

--- DividingNumbersWithoutDividingOperator.pas
+++ DividingNumbersWithoutDividingOperator.pas
@@ -1,19 +1,25 @@
-﻿program DividingNumbersWithoutDividingOperator;
+program DividingNumbersWithoutDividingOperator;
 
 var
-  n, m, b: Real;
-  
+  n, m, b: Real;  // Declare three variables of type Real: n, m, and b.
+
 Begin
-  Write('n va m ni kiriting: ');  ReadLn(n, m);
+  // Prompt the user to enter values for n and m.
+  Write('n va m ni kiriting: ');  
+  ReadLn(n, m);  // Read the values for n and m from the user.
   
-  b := 0; // initsializatsiya
-  
+  b := 0;  // Initialize b to 0. This will store the quotient of the division.
+
+  // As long as n is greater than or equal to m, keep subtracting m from n.
   while n >= m do
   begin 
-    b := b + 1; 
-    n := n - m;
+    b := b + 1;  // Increment the quotient (b) by 1.
+    n := n - m;  // Subtract m from n. This simulates the division process.
   end;
   
+  // Display the results.
+  // b now contains the quotient (whole part of the division).
+  // n now contains the remainder (fractional part of the division).
   WriteLn('Butun qismi: ', b, '; Qoldiq qismi: ', n);
 
-End.  
+End.
